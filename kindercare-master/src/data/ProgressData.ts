f3823c4e6d96b56b8842f2ab16bc4a9c2e7e6b02
Rangga Agastya <rangga.agastya@jagoanhosting.com>
2025-06-19 21:25:08 +0700

    [FEATURE] PROGRESS COMPLETION

---
+++ kindercare-master/src/data/ProgressData.ts
@@ -0,0 +1,39 @@
+import { Statistics, Materi } from "@/app/progress/page"
+
+export const statistics: Statistics[] = [
+    {
+        "title": "Runtutan Hari",
+        "value": 5,
+        "icon": "/icon/streak.svg"
+    },
+    {
+        "title": "Pembelajaran Hari Ini",
+        "value": "46 min",
+        "icon": "/icon/learning.svg"
+    },
+    {
+        "title": "Webinar Diikuti",
+        "value": 3,
+        "icon": "/icon/webinar.svg"
+    },
+    {
+        "title": "Materi Terselesaikan",
+        "value": 10,
+        "icon": "/icon/lesson.svg"
+    }
+]
+
+export const materi: Materi[] = [
+    {
+        "title": "Tubuh Milik Kita",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        "title": "Hidup Sehat",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        "title": "Tidur Teratur",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+]
